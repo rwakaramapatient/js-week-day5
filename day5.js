a1eac2be6e58043d64709823a1131b1aba118c97
rwakaramapatient <thesecond1018@gmail.com>
2026-02-16 07:48:51 +0200

    Commit on functions

---
+++ day5.js
@@ -0,0 +1,26 @@
+console.log("Day 5 start");
+const add = (a,b) => a+b;
+const subtract = (a,b) => a-b;
+const multiply = (a,b) => a*b;
+const divide = (a,b) => {
+    if (a === 0){
+        return null;
+    }
+    else {
+        return a/b;
+    }
+}
+const calcDiscount =(price,percent) => {
+    const discount = (percent/100)*price;
+    const newPrice = price - discount;
+    return {newPrice , discount}}
+console.log( add(1,2));
+console.log( add(4,2));
+console.log( subtract(2,1));
+console.log( subtract(3,1));
+console.log( multiply(1,2));
+console.log( multiply(1,3));
+console.log( divide(1,0));
+console.log( divide(6,3));
+console.log( calcDiscount(100,2));
+console.log( calcDiscount(500,4));
